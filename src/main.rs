1ccece144426ccbb7a9fb47e9745860c993099ae
Quinn Parrott <quinn.parrott@gmail.com>
2023-02-15 16:46:14 -0500

    Removed lots of useless code

--- src/main.rs
+++ src/main.rs
@@ -12,10 +12,9 @@ impl<'a, T> StackList<'a, T> {
     }
 }
 
-struct StackListIter<'a, T>(Option<&'a Node<'a, T>>); // TODO: Necessary? Make it not copy?
+struct StackListIter<'a, T>(Option<&'a Node<'a, T>>);
 
 impl<'a, T> Iterator for StackListIter<'a, T> {
-    // Directly walkable a good idea? No `.iter()`? When copy it is hard to use?
     type Item = &'a T;
 
     fn next(&mut self) -> Option<Self::Item> {
@@ -34,92 +33,10 @@ struct Node<'a, T> {
     value: T,
 }
 
-struct E<'a, T> {
-    // previous: Option<&'a mut E<'a, T>>,
-    previous: Option<&'a E<'a, T>>,
-    value: T,
-}
-
-// struct StackList<'a, 'b, T> {
-//     last_node: &'a mut Option<&'b mut Node<'b, T>>,
-// }
-// fn minimize1<'a, T>(node: &'a mut Node<'a, T>) -> ()
-// where
-//     T: Default,
-// {
-//     {
-//         let mut node_inner = E {
-//             previous: node,
-//             value: Default::default(),
-//         };
-//         {
-//             let e = &node_inner;
-//             {
-//                 minimize(Some(e))
-//             }
-//         }
-//     }
-// }
-
-struct Pointer<'a, T>(&'a mut T);
-
-fn a3<'a, 'b, T>(a: Pointer<'a, T>, b: &'b mut T) -> &'b mut T
-where
-    'a: 'b,
-{
-    let f: Pointer<'b, T> = a;
-    f.0
-}
-
-fn a2<'a, 'b, T>(a: &'a mut T, b: &'b mut T) -> &'b mut T
-where
-    'a: 'b,
-{
-    let f: Pointer<'b, T> = Pointer::<'a, T>(a);
-    f.0
-}
-
-fn a1<'a, 'b, T>(a: &'a mut T, b: &'b mut T) -> &'b mut T
-where
-    'a: 'b,
-{
-    let a1: &'a mut T = a;
-    let a2: &'b mut T = a1;
-    a2
-}
-
-fn minimize<'a, T>(node: Option<&'a E<'a, T>>) -> ()
-where
-    T: Default,
-{
-    {
-        let mut node_inner = E {
-            previous: node,
-            value: Default::default(),
-        };
-        {
-            let e = &node_inner;
-            {
-                minimize(Some(e))
-            }
-        }
-    }
-}
-
-// fn start_list<'a, 'b, T, U>(
 fn start_list<'a, T, U>(
     mut fun: impl for<'c> FnMut(StackList<'c, T>) -> Op<T, U>,
-    // mut fun: impl for<'a_1, 'a_2> FnMut(&mut StackList<'a_1, 'a_2, T>) -> Op<T, U>,
     node: Option<&'a Node<'a, T>>,
-) -> U
-// where
-//     'a: 'b,
-    // fn start_list<'a, T, U>(
-    //     mut fun: impl FnMut() -> Op<T, U>,
-    //     // mut fun: impl for<'a_1, 'a_2> FnMut(&mut StackList<'a_1, 'a_2, T>) -> Op<T, U>,
-    //     node: Option<&'a mut Node<'a, T>>,
-    // ) -> U
-{
+) -> U {
     match fun(StackList(node)) {
         Op::Store(store_val) => {
             let node_inner = Node {
@@ -133,21 +50,8 @@ fn start_list<'a, T, U>(
     }
 }
 
-fn start_list_empty<T, U>(
-    mut fun: impl for<'c> FnMut(StackList<'c, T>) -> Op<T, U>,
-    // fun: impl for<'a_1, 'a_2> FnMut(&mut StackList<'a_1, 'a_2, T>) -> Op<T, U>,
-) -> U {
+fn start_list_empty<T, U>(fun: impl for<'c> FnMut(StackList<'c, T>) -> Op<T, U>) -> U {
     start_list(fun, None)
-    // match fun() {
-    //     Op::Store(store_val) => {
-    //         let mut node_inner = Node {
-    //             previous: None,
-    //             value: store_val,
-    //         };
-    //         start_list(fun, &mut node_inner)
-    //     }
-    //     Op::Return(return_val) => return_val,
-    // }
 }
 
 fn main() {
